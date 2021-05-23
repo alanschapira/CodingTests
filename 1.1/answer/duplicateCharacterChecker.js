eeec22fad771e07942cfb98583c1c1202d463291
Alan Schapira <alonrules@hotmail.com>
2021-05-23 21:48:07 +0100

    a few more for duplicate character

--- 1.1/answer/duplicateCharacterChecker.js
+++ 1.1/answer/duplicateCharacterChecker.js
@@ -21,6 +21,33 @@ const duplicateCharacterChecker2 = (stringToCheck) => {
 
 const duplicateCharacterChecker3 = (stringToCheck) => {
     const checker = new Set();
+    let isDuplicate = false;
+    stringToCheck.split('').forEach(letter => {
+        if (checker.has(letter)) {
+            isDuplicate = true;
+        } else {
+            checker.add(letter);
+        }
+    });
+    return isDuplicate;
+};
+
+const duplicateCharacterChecker4 = (stringToCheck) => {
+    const checker = new Set();
+    const exists = [...stringToCheck].some(letter => {
+        if (checker.has(letter)) {
+            return true;
+        }
+        checker.add(letter);
+    });
+    return exists;
+};
+
+const duplicateCharacterChecker5 = (stringToCheck) => {
+    if (stringToCheck.length > 128) { // ascii only
+        return true;
+    }
+    const checker = new Set();
     const exists = [...stringToCheck].some(letter => {
         if (checker.has(letter)) {
             return true;
