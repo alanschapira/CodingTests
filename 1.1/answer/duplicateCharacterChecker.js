bdef2a40979ff0a396e5fba24da4f8982bd0b2c4
Alan Schapira <alanschapira@gmail.com>
2021-05-26 21:43:50 +0100

    Added for of loop for cleanest option

--- 1.1/answer/duplicateCharacterChecker.js
+++ 1.1/answer/duplicateCharacterChecker.js
@@ -1,58 +1,74 @@
 const duplicateCharacterChecker1 = (stringToCheck) => {
-    for (let i = 0; i <= stringToCheck.length; i++) {
-        for (let j = 0; j <= stringToCheck.length; j++) {
-            if (i !== j && stringToCheck[i] === stringToCheck[j]) {
-                return true;
-            }
-        }
+  for (let i = 0; i <= stringToCheck.length; i++) {
+    for (let j = 0; j <= stringToCheck.length; j++) {
+      if (i !== j && stringToCheck[i] === stringToCheck[j]) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 };
 
 const duplicateCharacterChecker2 = (stringToCheck) => {
-    return [...stringToCheck].some((letter, index) => {
-        for (let i = 0; i <= stringToCheck.length; i++) {
-            if (i !== index && letter === stringToCheck[i]) {
-                return true
-            }
-        }
-    });
+  return [...stringToCheck].some((letter, index) => {
+    for (let i = 0; i <= stringToCheck.length; i++) {
+      if (i !== index && letter === stringToCheck[i]) {
+        return true;
+      }
+    }
+  });
 };
 
 const duplicateCharacterChecker3 = (stringToCheck) => {
-    const checker = new Set();
-    let isDuplicate = false;
-    stringToCheck.split('').forEach(letter => {
-        if (checker.has(letter)) {
-            isDuplicate = true;
-        } else {
-            checker.add(letter);
-        }
-    });
-    return isDuplicate;
+  const checker = new Set();
+  let isDuplicate = false;
+  stringToCheck.split("").forEach((letter) => {
+    if (checker.has(letter)) {
+      isDuplicate = true;
+    } else {
+      checker.add(letter);
+    }
+  });
+  return isDuplicate;
 };
 
 const duplicateCharacterChecker4 = (stringToCheck) => {
-    const checker = new Set();
-    const exists = [...stringToCheck].some(letter => {
-        if (checker.has(letter)) {
-            return true;
-        }
-        checker.add(letter);
-    });
-    return exists;
+  const checker = new Set();
+  const exists = [...stringToCheck].some((letter) => {
+    if (checker.has(letter)) {
+      return true;
+    }
+    checker.add(letter);
+  });
+  return exists;
 };
 
 const duplicateCharacterChecker5 = (stringToCheck) => {
-    if (stringToCheck.length > 128) { // ascii only
-        return true;
+  const checker = new Set();
+  let duplicateExists = false;
+  for (const letter of stringToCheck) {
+    if (checker.has(letter)) {
+      duplicateExists = true;
+      break;
     }
-    const checker = new Set();
-    const exists = [...stringToCheck].some(letter => {
-        if (checker.has(letter)) {
-            return true;
-        }
-        checker.add(letter);
-    });
-    return exists;
+    checker.add(letter);
+  }
+  return duplicateExists;
+};
+
+const duplicateCharacterChecker6 = (stringToCheck) => {
+  // ascii only
+  if (stringToCheck.length > 128) {
+    return true;
+  }
+  const checker = new Set();
+  let duplicateExists = false;
+  for (const letter of stringToCheck) {
+    if (checker.has(letter)) {
+      duplicateExists = true;
+      break;
+    }
+    checker.add(letter);
+  }
+  return duplicateExists;
 };
--- 1.1/duplicateCharacterChecker.test.js
+++ 1.1/duplicateCharacterChecker.test.js
@@ -1,17 +1,24 @@
 const { duplicateCharacterChecker } = require("./duplicateCharacterChecker");
 
-describe('duplicateCharacterChecker', () => {
-    it('should return true for "abcdefghijklmnopqrstuvqxyza"', () => {
-        expect(duplicateCharacterChecker("abcdefghijklmnopqrstuvqxyza")).toBeTruthy()
-    })
-    it('should return true for "!abcd!"', () => {
-        expect(duplicateCharacterChecker("abcdefghijklmnopqrstuvqxyza")).toBeTruthy()
-
-    })
-    it('should return false for "asd"', () => {
-        expect(duplicateCharacterChecker("asd")).toBeFalsy()
-    })
-    it('should return false for all ascii', () => {
-        expect(duplicateCharacterChecker(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")).toBeFalsy()
-    })
-})
+describe("duplicateCharacterChecker", () => {
+  it('should return true for "abcdefghijklmnopqrstuvqxyza"', () => {
+    expect(duplicateCharacterChecker("abcdefghijklmnopqrstuvqxyza")).toEqual(
+      true
+    );
+  });
+  it('should return true for "!abcd!"', () => {
+    expect(duplicateCharacterChecker("abcdefghijklmnopqrstuvqxyza")).toEqual(
+      true
+    );
+  });
+  it('should return false for "asd"', () => {
+    expect(duplicateCharacterChecker("asd")).toEqual(false);
+  });
+  it("should return false for all ascii", () => {
+    expect(
+      duplicateCharacterChecker(
+        " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+      )
+    ).toEqual(false);
+  });
+});
